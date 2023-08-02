9a490d273a44a1a6693629180d402deab080f06e
LY <phamthily2222002@gmail.com>
2023-08-02 20:32:42 +0700

    nemo update 14-008 done

--- ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Setting.java
+++ ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Setting.java
@@ -1,6 +1,5 @@
 package com.shopme.common.entity;
 
-import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
---
+++ ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/countries_setting.js
@@ -0,0 +1,174 @@
+var buttonLoad;
+var dropDownCountry;
+var buttonAddCountry;
+var buttonUpdateCountry;
+var buttonDeleteCountry;
+var labelCountryName;
+var fieldCountryName;
+var seletedCountryName;
+var fieldCoutryCode;
+
+$(document).ready(function(){
+	buttonLoad = $("#buttonLoadCoutries");
+	dropDownCountry = $("#dropDownCountries");
+	buttonAddCountry = $("#buttonAddCountry");
+	buttonUpdateCountry = $("#buttonUpdateCountry");
+	buttonDeleteCountry = $("#buttonDeleteCountry");
+	labelCountryName = $("#labelCountryName");
+	fieldCountryName = $("#fieldCountryName");
+	fieldCoutryCode = $("#fieldCoutryCode");
+
+	
+	buttonLoad.click(function(){
+		loadCountries();
+	});
+	
+	dropDownCountry.on("change",function(){
+		changeFormStateToSelectedCountry();
+	});
+	
+	buttonAddCountry.click(function(){
+		if(buttonAddCountry.val() == "Add"){
+			addCountry();
+		}else{
+			changeFormStateToNew();
+		}
+	});
+	
+	buttonUpdateCountry.click(function(){
+		updateCountry();
+	});
+	
+	buttonDeleteCountry.click(function(){
+		deletedCountry();
+	});
+});
+
+function deletedCountry(){
+	optionValue = dropDownCountry.val();
+	countryId = optionValue.split("-")[0];
+	
+	url = contextPath + "countries/delete/" + countryId;
+	
+	$.get(url, function(){
+		$("#dropDownCountries option[value= '" + optionValue + "']").remove();
+		changeFormStateToNew();
+		
+	}).done(function(){
+		buttonLoad.val("Refesh Country List");
+		showToastMessage("All countries have been deleted");
+	}).fail(function(){
+		showToastMessage("ERROR: Could not connect to server or server encountered an error");
+	});
+}
+
+function updateCountry(){
+	url = contextPath +"countries/save";
+	countryName = fieldCountryName.val();
+	countryCode= fieldCoutryCode.val();
+	
+	countryId = dropDownCountry.val().split("-")[0];
+	
+	jsonData = {id: countryId,name: countryName,code:countryCode};
+	
+	$.ajax({
+		type:'POST',
+		url: url,
+		beforeSend: function(xhr){
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		},
+		data: JSON.stringify(jsonData),
+		contentType: 'application/json'
+	}).done(function(countryId){ 
+		$("#dropDownCountries option:selected").val(countryId + "-" +countryCode);
+		$("#dropDownCountries option:selected").text(countryName);
+		showToastMessage("The country has been updated");
+		
+		changeFormStateToNew();
+	}).fail(function(){
+		showToastMessage("ERROR: Could not connect to server or server encountered an error");
+	});
+}
+
+function addCountry(){
+	url = contextPath +"countries/save";
+	countryName = fieldCountryName.val();
+	countryCode= fieldCoutryCode.val();
+	
+	jsonData = {name: countryName,code:countryCode};
+	
+	$.ajax({
+		type:'POST',
+		url: url,
+		beforeSend: function(xhr){
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		},
+		data: JSON.stringify(jsonData),
+		contentType: 'application/json'
+	}).done(function(countryId){
+		selectNewAddedCountry(countryId, countryCode, countryName);
+		showToastMessage("The new country has been added");
+	}).fail(function(){
+		showToastMessage("ERROR: Could not connect to server or server encountered an error");
+	});
+
+}
+
+function selectNewAddedCountry(countryId, countryCode, countryName){
+	optionValue = countryId +"-"+countryCode;
+	$("<option>").val(optionValue).text(countryName).appendTo(dropDownCountry);
+	
+	$("#dropDownCountries option[value= '" + optionValue + "']").prop("selected",true);
+	fieldCoutryCode.val("");
+	fieldCountryName.val("").focus();
+}
+
+function changeFormStateToNew(){
+	buttonAddCountry.val("Add");
+	labelCountryName.text("Country Name: ");
+	
+	buttonUpdateCountry.prop("disabled",true);
+	buttonDeleteCountry.prop("disabled",true);
+	
+	fieldCoutryCode.val("");
+	fieldCountryName.val("").focus();
+}
+
+function changeFormStateToSelectedCountry(){
+	buttonAddCountry.prop("value","New");
+	buttonUpdateCountry.prop("disabled",false);
+	buttonDeleteCountry.prop("disabled",false);
+
+	labelCountryName.text("Seleted Country:");
+	
+	seletedCountryName = $("#dropDownCountries option:selected").text();
+	fieldCountryName.val(seletedCountryName);
+	
+	countryCode = dropDownCountry.val().split("-")[1];
+	fieldCoutryCode.val(countryCode);
+	
+	
+	
+}
+
+function loadCountries(){
+	url = contextPath+ "countries/list";
+	$.get(url, function(responseJSON){
+		dropDownCountry.empty();
+		
+		$.each(responseJSON, function(index, country){
+			optionValue = country.id + "-" + country.code;
+			$("<option>").val(optionValue).text(country.name).appendTo(dropDownCountry);
+		});
+	}).done(function(){
+		buttonLoad.val("Refesh Country List");
+		showToastMessage("All countries have been loaded");
+	}).fail(function(){
+		showToastMessage("ERROR: Could not connect to server or server encountered an error");
+	});
+}
+
+function showToastMessage(message){
+	$("#toastMessage").text(message);
+	$(".toast").toast('show');
+}

7086ef728ee8ea969de693a87fe5587f71e3d422
LY <phamthily2222002@gmail.com>
2023-08-11 22:05:42 +0700

    nemo update 17-010,011 done

--- ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -152,12 +152,20 @@ public class ProductController {
 	}
 	@GetMapping("/products/edit/{id}")
 	public String editProduct(@PathVariable("id") Integer id,Model model,
-			RedirectAttributes ra) {
+			RedirectAttributes ra,@AuthenticationPrincipal ShopmeUserDetails loggedUser) {
 		try {
 			Product product = productService.get(id);
 			List<Brand> listBrands = brandService.listAll();
 			Integer numberOfExitingExtraImages = product.getImages().size();
 
+			boolean isReadOnlyForSalesPerson = false;
+			if(!loggedUser.hasRole("Admin") && !loggedUser.hasRole("Editor")) {
+				if(loggedUser.hasRole("Salesperson")) {
+					isReadOnlyForSalesPerson = true;
+				}
+				
+			}
+			model.addAttribute("isReadOnlyForSalesPerson", isReadOnlyForSalesPerson);
 			model.addAttribute("product", product);
 			model.addAttribute("listBrands", listBrands);
 			model.addAttribute("pageTitle", "Edit Product (ID: " + id + ")");

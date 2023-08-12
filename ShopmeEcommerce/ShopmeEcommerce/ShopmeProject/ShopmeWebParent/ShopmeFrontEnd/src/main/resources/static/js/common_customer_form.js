d54edd575ad6b28d6cd55d889e16e04d282b94bf
LY <phamthily2222002@gmail.com>
2023-08-12 10:26:01 +0700

    nemo update 18-003 done

--- ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/common_customer_form.js
+++ ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/common_customer_form.js
@@ -34,16 +34,3 @@ $(document).ready(function(){
 			confirmPassword.setCustomValidity("");
 		}
 	}
-function showModalDialog(title, message){
-		$("#modalTitle").text(title);
-		$("#modalBody").text(message);
-		$("#modalDialog").modal();
-	}
-	
-	function showErrorModal(message){
-		showModalDialog("Error", message);
-	}
-	
-	function showWarningModal(message){
-		showModalDialog("Warning", message);
-	}

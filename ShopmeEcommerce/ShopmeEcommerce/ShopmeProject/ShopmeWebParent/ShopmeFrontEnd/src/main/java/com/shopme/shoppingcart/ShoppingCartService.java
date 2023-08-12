31ed7531348b79c323d2ae95a8cee58400aa4d2f
LY <phamthily2222002@gmail.com>
2023-08-12 20:00:52 +0700

    nemo update 18-006 done

--- ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
+++ ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
@@ -8,11 +8,16 @@ import org.springframework.stereotype.Service;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.Product;
+import com.shopme.product.ProductRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
+@Transactional
 public class ShoppingCartService {
 
 	@Autowired private CartItemRepository cartRepo;
+	@Autowired private ProductRepository productRepo;
 	
 	public Integer addProduct(Integer productId,Integer quantity,Customer customer) throws ShoppingCartException {
 		
@@ -42,5 +47,13 @@ public class ShoppingCartService {
 	
 	public List<CartItem> listCartItems(Customer customer){
 		return cartRepo.findByCustomer(customer);
+	}
+	
+	public float updateQuantity(Integer productId,Integer quantity,Customer customer) {
+		
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+		return subtotal;
 	}
 }

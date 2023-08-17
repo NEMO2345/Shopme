7422f169050650bae09dbc5150452595a4f833b4
LY <phamthily2222002@gmail.com>
2023-08-17 21:05:29 +0700

    nemo update 23-005 done

--- ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
+++ ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
@@ -8,8 +8,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.aspectj.weaver.patterns.ThisOrTargetAnnotationPointcut;
-
 import com.shopme.common.entity.AbstractAddress;
 import com.shopme.common.entity.Address;
 import com.shopme.common.entity.Customer;

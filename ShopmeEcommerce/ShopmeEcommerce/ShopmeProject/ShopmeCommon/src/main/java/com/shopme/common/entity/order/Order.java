10fd9f002ef79a77910d0e59eb668976dcdf067c
LY <phamthily2222002@gmail.com>
2023-08-21 22:53:29 +0700

    nemo update 25-001,002 done

--- ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
+++ ShopmeEcommerce/ShopmeEcommerce/ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
@@ -316,6 +316,12 @@ public class Order extends AbstractAddress{
 	public boolean isDelivered() {
 		return hasStatus(OrderStatus.DELIVERED);
 	}
+	
+	@Transient 
+	public boolean isReturnRequested() {
+		return hasStatus(OrderStatus.RETURN_REQUESTED);
+	}
+	
 	@Transient
 	public boolean isReturned() {
 		return hasStatus(OrderStatus.RETURNED);
@@ -329,4 +335,19 @@ public class Order extends AbstractAddress{
 		}
 		return false;
 	}
+	
+	 @Transient
+	 public String getProductNames() {
+		 String productNames = "";
+		 
+		 productNames = "<ul>";
+		 
+		 for(OrderDetail detail : orderDetails) {
+			 productNames += "<li>" + detail.getProduct().getShortName() + "</li>";
+ 		 }
+		 productNames += "</ul>";
+		 
+		 return productNames;
+		 
+	 }
 }
